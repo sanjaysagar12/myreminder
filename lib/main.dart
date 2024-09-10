126d2d02eb3711501a6a8e4145d7f2f8057ee2bf
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-09-10 22:05:32 +0530

    Added Fonts
    JosefinSans,PlaywriteCU and Handjet are added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("MyReminder"),
-        ),
-        body: Center(
-          child: Text("MY REMINDER"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text("+"),
-          onPressed: null,
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyReminder"),
+      ),
+      body: Center(
+        child: Text(
+          "My Reminder",
+          style: TextStyle(fontFamily: "Handjet"),
         ),
       ),
-    ),
-  );
+      floatingActionButton: FloatingActionButton(
+        child: Text("+"),
+        onPressed: null,
+      ),
+    );
+  }
 }
